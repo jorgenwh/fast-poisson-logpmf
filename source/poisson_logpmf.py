c52a0cd19361439be3e2092cc885f304ed956cbf
jorgenwh <jorgenwhen@gmail.com>
2022-11-24 14:21:24 +0100

    a

--- source/poisson_logpmf.py
+++ source/poisson_logpmf.py
@@ -18,16 +18,51 @@ def cp_poisson_logpmf(k, r):
     if isinstance(k, np.ndarray) and isinstance(r, np.ndarray):
         k_ = cp.asarray(k)
         r_ = cp.asarray(r)
-        ret = k_ * cp.log(r_) - r_ - cupyx.scipy.special.gammaln(k_+1)
+
+        temp = cp.log(r_)
+        temp = k_ * temp
+        temp = temp - r_
+        temp2 = k_ + 1
+        temp2 = cupyx.scipy.special.gammaln(temp2)
+        ret = temp - temp2
+
+        #ret = k_ * cp.log(r_) - r_ - cupyx.scipy.special.gammaln(k_+1)
         return cp.asnumpy(ret)
     if isinstance(k, cp.ndarray) and isinstance(r, np.ndarray):
         r_ = cp.asarray(r)
-        return k * cp.log(r_) - r_ - cupyx.scipy.special.gammaln(k+1)
+
+        temp = cp.log(r_)
+        temp = k * temp
+        temp = temp - r_
+        temp2 = k + 1
+        temp2 = cupyx.scipy.special.gammaln(temp2)
+        ret = temp - temp2
+
+        #return k * cp.log(r_) - r_ - cupyx.scipy.special.gammaln(k+1)
+        return ret
     if isinstance(k, np.ndarray) and isinstance(r, cp.ndarray):
         k_ = cp.asarray(k)
-        return k_ * cp.log(r) - r - cupyx.scipy.special.gammaln(k_+1)
+
+        temp = cp.log(r)
+        temp = k_ * temp
+        temp = temp - r
+        temp2 = k_ + 1
+        temp2 = cupyx.scipy.special.gammaln(temp2)
+        ret = temp - temp2
+
+        #return k_ * cp.log(r) - r - cupyx.scipy.special.gammaln(k_+1)
+        return ret
     if isinstance(k, cp.ndarray) and isinstance(r, cp.ndarray):
-        return k * cp.log(r) - r - cupyx.scipy.special.gammaln(k+1)
+
+        temp = cp.log(r)
+        temp = k * temp
+        temp = temp - r
+        temp2 = k + 1
+        temp2 = cupyx.scipy.special.gammaln(temp2)
+        ret = temp - temp2
+
+        #return k * cp.log(r) - r - cupyx.scipy.special.gammaln(k+1)
+        return ret
     return NotImplemented
 
 def cuda_poisson_logpmf(k, r):
